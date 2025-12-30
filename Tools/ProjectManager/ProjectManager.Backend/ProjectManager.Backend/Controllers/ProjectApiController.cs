8057b7b42c37506ef10680343dfa820469d57793
Samuel Blake <sam_blake94@hotmail.co.uk>
2025-12-30 23:22:58 +0000

    Adding the bare minimum project.

---
+++ Tools/ProjectManager/ProjectManager.Backend/ProjectManager.Backend/Controllers/ProjectApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectManager.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjectApiController : ControllerBase
+    {
+        [HttpPost(nameof(Handshake))]
+        public IActionResult Handshake()
+        {
+            //return NotFound(); 
+            return Ok("Handshake successful");
+        }
+    }
+}
---
+++ Tools/ProjectManager/ProjectManager.Backend/ProjectManager.Backend/Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddCors();
+
+builder.WebHost.UseUrls("https://localhost:5005", "http://localhost:5004");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors(builder =>
+    builder.AllowAnyOrigin()
+           .AllowAnyHeader()
+           .AllowAnyMethod()
+);
+
+app.Run();
